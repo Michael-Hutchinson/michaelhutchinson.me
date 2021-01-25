08e4951398cf5607c84ce588a1aa143766964de4
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2021-01-25 23:00:42 +0000

    Add typerJS and create home section for website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header"
+import Home from "./components/Home"
 import Footer from "./components/Footer"
 
 function App() {
   return (
     <div className="portfolio">
       <Header />
+      <Home />
       <Footer />
     </div>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import Typed from 'react-typed'
+
+const introText = [
+  `I am a creative designer and web developer based in Manchester.`
+];
+
+function Home() {
+  return (
+    <section id="home">
+      <h5>hey there, this is my portfolio website.</h5>
+      <h1><Typed strings={introText} typeSpeed={60} /></h1>
+    </section>
+  );
+}
+
+export default Home;
