ffd125de8445568cc721cda66251019e5521c4a1
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2021-01-27 18:56:23 +0000

    Remove scss files and have everything in styled-components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import Header from "./components/Header"
-import Home from "./components/Home"
-import Footer from "./components/Footer"
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
 
-import {createGlobalStyle, ThemeProvider} from 'styled-components'
-import * as theme from './styles/theme'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import * as theme from "./styles/theme";
 
 const GlobalStyle = createGlobalStyle`
   @import url("https://fonts.googleapis.com/css?family=Karla:400,400i");
   html,
   body {
-    font-family: ${props => props.theme.font};
+    font-family: ${(props) => props.theme.font};
     margin: 0;
   }
   * {
@@ -22,7 +22,27 @@ const GlobalStyle = createGlobalStyle`
   ::-webkit-scrollbar-thumb {
     background: black;
   }
-`
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    color: ${(props) => props.theme.empressGrey};
+    font-weight: normal;
+    line-height: normal;
+    letter-spacing: 0.5px;
+  }
+  p {
+    color: ${(props) => props.theme.empressGrey};
+    line-height: 25px;
+  }
+  a {
+    color: black;
+    transition: 0.5s;
+    text-decoration: none;
+  }
+`;
 
 function App() {
   return (
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,17 @@
-import React from 'react'
-import Typed from 'react-typed'
-import styled from 'styled-components'
-import '../styles/default.scss';
-
-const Title = styled.h1`
-  color: ${props => props.theme.empressGrey};
-  font-weight: normal;
-  line-height: normal;
-  letter-spacing: 0.5px;
-`;
+import React from "react";
+import Typed from "react-typed";
 
 const introText = [
-  `I am a creative designer and web developer based in Manchester.`
+  `I am a creative designer and web developer based in Manchester.`,
 ];
 
 function Home() {
   return (
     <section id="home">
       <h5>hey there, this is my portfolio website.</h5>
-      <Title>
+      <h1>
         <Typed strings={introText} typeSpeed={60} />
-      </Title>
+      </h1>
     </section>
   );
 }
