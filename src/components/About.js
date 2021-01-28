7ce6a0f8dc7d252b41f88269a4475824356d35c5
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2021-01-28 23:33:34 +0000

    Get wording from json file instead

--- src/components/About.js
+++ src/components/About.js
@@ -1,24 +1,13 @@
 import React from 'react';
+import wording from '../data/wording.json';
 
 function About() {
   return (
     <section id="about">
-      <h5>about me</h5>
-      <h2>
-        My name is Michael Hutchinson. A web designer and web developer based in
-        Manchester.
-      </h2>
-      <p>
-        Experienced Web Developer with a history of working in the IT industry.
-        Skilled in Web Design & Development including HTML, CSS & Javascript.
-        Strong UX portfolio with a Masters degree in Information Technology from
-        Leeds University.
-      </p>
-      <p>
-        If you want to know more just email me at
-        michael-hutchinson@hotmail.co.uk or use the contact form at the bottom
-        of the page.
-      </p>
+      <h5>{wording[1].title}</h5>
+      <h2>{wording[1].h2}</h2>
+      <p>{wording[1].p1}</p>
+      <p>{wording[1].p2}</p>
     </section>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import Typed from 'react-typed';
 import Button from './shared/Button';
-
-const introText = [
-  `I am a creative designer and web developer based in Manchester.`,
-];
+import wording from '../data/wording.json';
 
 function Home() {
   return (
     <section id="home">
-      <h5>hey there, this is my portfolio website.</h5>
+      <h5>{wording[0].title}</h5>
       <h1>
-        <Typed strings={introText} typeSpeed={60} />
+        <Typed strings={wording[0].h1} typeSpeed={60} />
       </h1>
       <Button />
     </section>
