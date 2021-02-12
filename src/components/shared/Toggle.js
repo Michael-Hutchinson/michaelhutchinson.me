c7ec8d6763142967ffc2763f21ea69dcecbc0300
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2021-02-12 18:28:18 +0000

    Tweak styling on toggle button to match other button

--- src/components/shared/Toggle.js
+++ src/components/shared/Toggle.js
@@ -3,15 +3,20 @@ import { func } from 'prop-types';
 import styled from 'styled-components';
 
 const Button = styled.a`
-  background: red;
-  border: 2px solid red;
-  color: black;
+  background-color: ${({ theme }) => theme.buttonBg};
+  color: ${({ theme }) => theme.buttonText};
+  border: 1px solid transparent;
   cursor: pointer;
-  position: absolute;
-  right: 22px;
-  bottom: 22px;
+  position: fixed;
+  right: 5%;
+  bottom: 5%;
   z-index: 10;
   outline: 0;
+  text-transform: uppercase;
+  cursor: pointer;
+  letter-spacing: 1.6px;
+  padding: 14px 25px;
+  border-radius: 100px;
 }
 `;
 
