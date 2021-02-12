283c2eed5ee259ebed6ad9aeaed03bcc682fc7aa
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2021-02-12 18:28:33 +0000

    Change button styling and add a hover state

--- src/components/shared/Button.js
+++ src/components/shared/Button.js
@@ -1,22 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FaArrowRight } from 'react-icons/fa';
 
 const Cta = styled.button`
   background-color: ${({ theme }) => theme.buttonBg};
   color: ${({ theme }) => theme.buttonText};
   letter-spacing: 1.6px;
-  padding: 14px 28px;
+  padding: 14px 25px;
   border-radius: 100px;
   outline: 0;
   text-transform: uppercase;
   cursor: pointer;
   border: 1px solid transparent;
-  font-size: 13px;
   font-family: 'Karla', sans-serif;
+  transition: all ease-in-out 0.4s;
+  svg {
+    margin-left: 10px;
+    vertical-align: top;
+  }
+  :hover {
+    transform: scale(1.1);
+    svg {
+      transition: all ease-in-out 0.4s;
+      transform: rotate(90deg);
+    }
+  }
 `;
 
 function Button() {
-  return <Cta>Learn More &gt;</Cta>;
+  return (
+    <Cta href="#about">
+      Learn More
+      <FaArrowRight />
+    </Cta>
+  );
 }
 
 export default Button;
