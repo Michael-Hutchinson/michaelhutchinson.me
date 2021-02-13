32929c2947240a7b50fbd439a301a29275ff6463
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2021-02-13 18:12:16 +0000

    update social imports

--- src/components/shared/Social.js
+++ src/components/shared/Social.js
@@ -1,56 +1,45 @@
 import React from 'react';
 import { FaTwitter, FaGithub, FaLinkedinIn } from 'react-icons/fa';
 import styled from 'styled-components';
+import config from '../../data/config.json';
 
-const Icon = styled.div`
-  height: 35px;
-  width: 35px;
-  background-color: ${({ theme }) => theme.buttonBg};
-  transition: all ease-in-out 0.4s;
-  padding: 10px;
-  border-radius: 100%;
-  display: inline-flex;
-  cursor: pointer;
-  :not(:last-child) {
-    margin-right: 5px;
-  }
-  svg {
-    color: ${({ theme }) => theme.buttonText};
-  }
-  :hover {
-    transform: scale(1.1);
-  }
-`;
+const Icon = styled.ul``;
 
 function Social() {
   return (
     <div>
       <Icon>
-        <a
-          href="https://www.twitter.com/itshutchy2"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaTwitter />
-        </a>
+        <li>
+          <a
+            href={config.socialMedia.twitter.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaTwitter />
+          </a>
+        </li>
       </Icon>
       <Icon>
-        <a
-          href="https://github.com/iTsHutchy"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaGithub />
-        </a>
+        <li>
+          <a
+            href={config.socialMedia.github.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub />
+          </a>
+        </li>
       </Icon>
       <Icon>
-        <a
-          href="https://www.linkedin.com/in/mhutchinson4/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaLinkedinIn />
-        </a>
+        <li>
+          <a
+            href={config.socialMedia.linkedin.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedinIn />
+          </a>
+        </li>
       </Icon>
     </div>
   );
