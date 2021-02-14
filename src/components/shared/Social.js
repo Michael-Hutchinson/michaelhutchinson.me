fc026ea76ecfcc4ece4ece959bf3f0931143a4cf
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2021-02-14 17:40:39 +0000

    Finish styling for social media icons

--- src/components/shared/Social.js
+++ src/components/shared/Social.js
@@ -3,11 +3,48 @@ import { FaTwitter, FaGithub, FaLinkedinIn } from 'react-icons/fa';
 import styled from 'styled-components';
 import config from '../../data/config.json';
 
-const Icon = styled.ul``;
+const Wrapper = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 20px;
+  z-index: 10;
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const Icon = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  &:after {
+    content: '';
+    width: 1px;
+    height: 100px;
+    background-color: ${({ theme }) => theme.text};
+  }
+  li {
+    &:last-of-type {
+      margin-bottom: 20px;
+    }
+    a {
+      padding: 10px;
+      display: flex;
+      transition: all ease-in-out 0.4s;
+      color: ${({ theme }) => theme.link};
+      &:hover {
+        transform: scale(1.5);
+      }
+    }
+  }
+`;
 
 function Social() {
   return (
-    <div>
+    <Wrapper>
       <Icon>
         <li>
           <a
@@ -18,8 +55,6 @@ function Social() {
             <FaTwitter />
           </a>
         </li>
-      </Icon>
-      <Icon>
         <li>
           <a
             href={config.socialMedia.github.url}
@@ -29,8 +64,6 @@ function Social() {
             <FaGithub />
           </a>
         </li>
-      </Icon>
-      <Icon>
         <li>
           <a
             href={config.socialMedia.linkedin.url}
@@ -41,7 +74,7 @@ function Social() {
           </a>
         </li>
       </Icon>
-    </div>
+    </Wrapper>
   );
 }
 
