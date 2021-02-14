02a7f49b0ac0cc6e78794ef976179e6d20c26e22
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2021-02-14 19:50:23 +0000

    Small button changes

--- src/components/shared/Button.js
+++ src/components/shared/Button.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Cta = styled.button`
+const Cta = styled.a`
   background-color: transparent;
-  color: ${({ theme }) => theme.link};
-  letter-spacing: 0.1rem;
-  padding: 0.875rem 1.563rem;
-  border-radius: 0.25rem;
-  outline: 0;
-  text-transform: uppercase;
-  cursor: pointer;
   border: 0.063rem solid ${({ theme }) => theme.link};
+  border-radius: 0.25rem;
+  color: ${({ theme }) => theme.link};
+  cursor: pointer;
+  display: inline-flex;
   font-family: var(--font-main);
+  font-size: 0.75rem;
+  letter-spacing: 0.1rem;
+  outline: 0;
+  padding: 0.875rem 1.563rem;
+  text-transform: uppercase;
   transition: all ease-in-out 0.2s;
   :hover {
     background-color: ${({ theme }) => theme.shadow};
