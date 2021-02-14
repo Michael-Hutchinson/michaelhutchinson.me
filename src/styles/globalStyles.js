c9c1dc34d4e4948d414a5ff09aa88fcb737dd01d
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2021-02-14 18:58:08 +0000

    Update global styles css

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -2,21 +2,37 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   @import url("https://fonts.googleapis.com/css?family=Karla:400,400i");
+
+  :root {
+    --black: #000000;
+    --black-shadow: rgba(0, 0, 0, 0.1);
+    --font-main: "Karla", sans-serif;
+    --font-links: "SF Mono", monospace;
+    --gold: #E3AE4A;
+    --gold-shadow: rgba(227, 174, 74, 0.1);
+    --light-navy: #233554;
+    --light-slate: #8892B0;
+    --navy: #0A192F;
+    --slate: #AFAFBF;
+    --white: #E6F1FF;
+  }
+
   html,
   body {
-    font-family: "Karla", sans-serif;
-    margin: 0;
     background: ${({ theme }) => theme.background};
+    font-family: var(--font-main);
+    margin: 0;
+    scroll-behavior: smooth;
   }
   * {
     box-sizing: border-box;
   }
   ::-webkit-scrollbar {
-    width: 8px;
-    height: 8px;
+    height: 5px;
+    width: 5px;
   }
   ::-webkit-scrollbar-thumb {
-    background: ${({ theme }) => theme.background};
+    background: ${({ theme }) => theme.title};
   }
   h1,
   h2,
@@ -30,16 +46,17 @@ const GlobalStyle = createGlobalStyle`
     letter-spacing: 0.5px;
   }
   p {
-    color: ${({ theme }) => theme.title};
+    color: ${({ theme }) => theme.text};
     line-height: 25px;
   }
   a {
-    color: ${({ theme }) => theme.title};
+    color: ${({ theme }) => theme.link};
     text-decoration: none;
   }
   section {
-    max-width: 75%;
-    margin: 0 auto;
+    margin: 0px auto;
+    max-width: 80%;
+    padding: 100px 0px;
   }
 `;
 
