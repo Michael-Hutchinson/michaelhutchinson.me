91d40d8e79a0b196e0a7ec3585dd920ed3bb1108
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2021-02-15 17:25:20 +0000

    Create toggle switch for dark mode instead of using an onclick button

--- src/components/shared/Toggle.js
+++ src/components/shared/Toggle.js
@@ -1,31 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { func } from 'prop-types';
-import styled from 'styled-components';
+import Switch from 'react-switch';
+import Darkmode from './Darkmode';
 
-const Button = styled.a`
-  background-color: ${({ theme }) => theme.buttonBg};
-  color: ${({ theme }) => theme.buttonText};
-  border: 1px solid transparent;
-  cursor: pointer;
-  position: fixed;
-  right: 5%;
-  bottom: 5%;
-  z-index: 10;
-  outline: 0;
-  text-transform: uppercase;
-  cursor: pointer;
-  letter-spacing: 1.6px;
-  padding: 14px 25px;
-  border-radius: 100px;
-}
-`;
-
-function Toggle({ toggleTheme }) {
-  return <Button onClick={toggleTheme}>Switch Theme</Button>;
+function Toggle(props) {
+  const [themeToggler] = Darkmode();
+  const [checked, setChecked] = useState(false);
+  useEffect(() => {
+    if (themeToggler === 'light') {
+      setChecked(false);
+    } else {
+      setChecked(true);
+    }
+  });
+  const handleChange = (nextChecked) => {
+    setChecked(nextChecked);
+    props.toggleTheme();
+  };
+  return <Switch onChange={handleChange} checked={checked} />;
 }
 
 Toggle.propTypes = {
-  toggleTheme: func.isRequired,
+  toggleTheme: func,
+};
+
+Toggle.defaultProps = {
+  toggleTheme: func,
 };
 
 export default Toggle;
