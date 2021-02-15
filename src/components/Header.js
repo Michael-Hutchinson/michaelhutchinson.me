0ed80dd42d5bbc18acea27d4856cb323551ef7f7
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2021-02-15 23:07:29 +0000

    move particles into app.js from header so it doesnt inherit header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 import React from 'react';
 import { func, string } from 'prop-types';
-import Particles from 'react-particles-js';
 import Navbar from './shared/Navbar';
 
 const Top = styled.header`
@@ -20,61 +19,6 @@ function Header(props) {
   return (
     <Top>
       <Navbar currentTheme={theme} toggleTheme={toggleTheme} />
-      <Particles
-        width="100vw"
-        height="100vh"
-        params={{
-          particles: {
-            number: {
-              value: 160,
-              density: {
-                enable: false,
-              },
-            },
-            size: {
-              value: 3,
-              random: true,
-              anim: {
-                speed: 4,
-                size_min: 0.3,
-              },
-            },
-            line_linked: {
-              enable: false,
-            },
-            move: {
-              random: true,
-              speed: 1,
-              direction: 'top',
-              out_mode: 'out',
-            },
-          },
-          interactivity: {
-            events: {
-              onhover: {
-                enable: true,
-                mode: 'bubble',
-              },
-              onclick: {
-                enable: true,
-                mode: 'repulse',
-              },
-            },
-            modes: {
-              bubble: {
-                distance: 250,
-                duration: 2,
-                size: 0,
-                opacity: 0,
-              },
-              repulse: {
-                distance: 400,
-                duration: 4,
-              },
-            },
-          },
-        }}
-      />
     </Top>
   );
 }
