7ba210d5355d164d63954846442a1f4bcf1daa28
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2021-02-17 21:15:21 +0000

    Update section titles to use title component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import config from '../data/config.json';
+import Title from './shared/Title';
 
 function Contact() {
   return (
     <section id="contact">
-      <h5>{config.sections.contact.title}</h5>
+      <Title titleText={config.sections.contact.title} />
       <h3>{config.sections.contact.h3}</h3>
     </section>
   );
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import config from '../data/config.json';
+import Title from './shared/Title';
 
 function Work() {
   return (
     <section id="work">
-      <h5>{config.sections.work.title}</h5>
+      <Title titleText={config.sections.work.title} />
       <h3>{config.sections.work.h3}</h3>
     </section>
   );
