f8732d16422e9b6f8cfa2a6235313e4df6674841
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2021-02-18 23:08:26 +0000

    Add some project images
    * need to finish styling on this and add responsive classes

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
 import config from '../data/config.json';
 import Title from './shared/Title';
+import images from '../images';
+
+const ImageWrap = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 1.25rem;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  border: 0.063rem solid ${({ theme }) => theme.link};
+  border-radius: 0.25rem;
+  padding: 0.313rem;
+`;
 
 function Work() {
   return (
     <section id="work">
       <Title titleText={config.sections.work.title} />
       <h3>{config.sections.work.h3}</h3>
+      <ImageWrap>
+        <Image src={images.aetc} alt="aetc" />
+        <Image src={images.betting} alt="betting" />
+        <Image src={images.leeds} alt="leeds" />
+        <Image src={images.lscb} alt="lscb" />
+        <Image src={images.morality} alt="morality" />
+        <Image src={images.portfolio} alt="portfolio" />
+        <Image src={images.runner} alt="runner" />
+      </ImageWrap>
     </section>
   );
 }
---
+++ src/images/index.js
@@ -0,0 +1,19 @@
+import aetc from './work/aetc-work.png';
+import betting from './work/betting-work.png';
+import leeds from './work/leeds-work.png';
+import lscb from './work/lscb-work.png';
+import morality from './work/morality-work.png';
+import portfolio from './work/portfolio-work.png';
+import runner from './work/runner-work.png';
+
+const images = {
+  aetc,
+  betting,
+  leeds,
+  lscb,
+  morality,
+  portfolio,
+  runner,
+};
+
+export default images;
