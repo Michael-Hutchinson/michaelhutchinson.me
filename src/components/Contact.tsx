5c1862281b645f863588fe1003f5bd051d7ee3f1
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-02-22 15:18:54 +0000

    add ts to contact

---
+++ src/components/Contact.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import config from '../data/config.json';
+import Title from './shared/Title';
+import Form from './shared/Form';
+
+const Contact: React.FunctionComponent = () => (
+  <section id="contact">
+    <Title titleText={config.sections.contact.title} />
+    <h3>{config.sections.contact.h3}</h3>
+    <Form />
+  </section>
+);
+
+export default Contact;
