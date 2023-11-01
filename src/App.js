2d09710d7df8d3dd71e60de1afa4713c6dc9bd07
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-11-01 21:49:03 +0000

    feat: readd particles as old package was deprecated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import Particles from 'react-tsparticles';
 import Header from './components/Header';
 import Home from './components/Home';
 import About from './components/About';
@@ -11,11 +12,75 @@ import GlobalStyle from './styles/globalStyles';
 import Darkmode from './components/shared/Darkmode';
 import { lightTheme, darkTheme } from './styles/theme';
 
-function App() {
+const App = () => {
   const [theme, themeToggler] = Darkmode();
   const themeMode = theme === 'light' ? lightTheme : darkTheme;
+
   return (
     <ThemeProvider theme={themeMode}>
+      <Particles
+        id="tsparticles"
+        options={{
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onhover: {
+                enable: true,
+                mode: 'bubble',
+              },
+              onclick: {
+                enable: true,
+                mode: 'repulse',
+              },
+            },
+            modes: {
+              bubble: {
+                distance: 250,
+                duration: 2,
+                size: 0,
+                opacity: 0,
+              },
+              repulse: {
+                distance: 400,
+                duration: 4,
+              },
+            },
+          },
+          particles: {
+            move: {
+              direction: 'none',
+              enable: true,
+              outModes: {
+                default: 'bounce',
+              },
+              random: true,
+              speed: 6,
+              straight: false,
+            },
+            number: {
+              value: 160,
+              density: {
+                enable: false,
+              },
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: 'circle',
+            },
+            size: {
+              value: 3,
+              random: true,
+              anim: {
+                speed: 4,
+                size_min: 0.3,
+              },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
       <GlobalStyle />
       <Header theme={theme} toggleTheme={themeToggler} />
       <main>
@@ -28,6 +93,6 @@ function App() {
       </main>
     </ThemeProvider>
   );
-}
+};
 
 export default App;
