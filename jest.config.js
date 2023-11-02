7a420630d2a31aa91a715dc872fc43a5eb64291f
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-11-02 22:47:00 +0000

    feat: update testing library packages

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,4 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
-  moduleNameMapper: {
-    '\\.css$': 'identity-obj-proxy',
-  },
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/'],
 };
